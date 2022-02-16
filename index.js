b5af03c7831778ad087d4958b72726c6804c671e
555_5 <82145708+ohseulbin@users.noreply.github.com>
2022-02-16 14:20:55 +0900

    layout_fullpage

---
+++ index.js
@@ -0,0 +1,47 @@
+// fullpage
+$(function(){
+    $('#fullpage').fullpage({
+      autoScrolling:true,
+      //scrollHorizontally: true,
+      showActiveTooltip:true,
+      scrollOverflow: true,
+      //메뉴 내비게이션
+      menu: '#menu',
+      anchors:['section1','section2','section3','section4','section5']
+    });
+  });
+  
+  // swiper
+  $(function(){
+    $("#youtube").YTPlayer();
+  });
+
+  const mainSlide = new Swiper('.mainSlide', {
+    pagination: {
+        el: '.swiper-pagination',
+        mousewheel: true,
+        keyboard: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    autoplay: {
+        delay: 2500
+    },
+    loop: true,
+    slidesPerView: 1, //화면 당 이미지 수
+    spaceBetween: 20, // 이미지 사이의 간격
+    // effect: 'flip', flipEffect: {   slideShadows: false, },
+});
+
+// 원하는 이미지로 이동(loop 버전)
+const menu = document.querySelectorAll('.menu');
+menu.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        mainSlide.slideToLoop(idx); // 해당 페이지로 이동
+        mainSlide
+            .autoplay
+            .start(); // 자동 시작
+    });
+});
